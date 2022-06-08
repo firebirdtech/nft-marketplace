aad395d67207a29cb85a28e9e0102c778f8a745a
Neel Ratan Guria <neelratan@live.com>
2022-06-08 07:01:22 +0530

    search

--- NftMarketplace/components/index.js
+++ NftMarketplace/components/index.js
@@ -2,7 +2,11 @@ import HomeHeader from './HomeHeader'
 import FocusedStatusBar from './FocusedStatusBar'
 import NFTCard from './NFTCard'
 import { CircleButton } from './Button'
+import DetailsBid from './DetailsBid'
+import DetailsDesc from './DetailsDesc'
+import { SubInfo } from './SubInfo'
+
 
 export {
-    HomeHeader, NFTCard, FocusedStatusBar, CircleButton
+    HomeHeader, NFTCard, FocusedStatusBar, CircleButton, DetailsDesc, DetailsBid, SubInfo
 }
--- NftMarketplace/screens/Home.js
+++ NftMarketplace/screens/Home.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import { useState } from 'react'
-import { Text, SafeAreaView, FlatList, View } from 'react-native'
+import { SafeAreaView, FlatList, View } from 'react-native'
 import { COLORS, NFTData } from '../constants'
 import { NFTCard, HomeHeader, FocusedStatusBar } from '../components'
 
 const Home = () => {
+  const [nftData, setNftData] = useState(NFTData)
+  const handleSearch = (value) => {
+
+    if (!value.length) return setNftData(NFTData)
+
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    )
+
+    if (filteredData.length) {
+      setNftData(filteredData)
+    } else {
+      setNftData(NFTData)
+    }
+  }
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar background={COLORS.primary} />
       <View style={{ flex: 1 }}>
         <View style={{ zIndex: 0 }}>
           <FlatList
-            data={NFTData}
-            renderItem={({ item }) => <NFTCard data={item}/>}
+            data={nftData}
+            renderItem={({ item }) => <NFTCard data={item} />}
             keyExtractor={(item) => item.id}
             showsHorizontalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader />}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
           />
         </View>
       </View>
@@ -27,10 +42,10 @@ const Home = () => {
         left: 0,
         zIndex: -1,
       }}>
-        <View style={{height: 300, backgroundColor: COLORS.primary}}>
+        <View style={{ height: 300, backgroundColor: COLORS.primary }}>
 
         </View>
-        <View style={{flex: 1, backgroundColor: COLORS.white}}>
+        <View style={{ flex: 1, backgroundColor: COLORS.white }}>
 
         </View>
       </View>
